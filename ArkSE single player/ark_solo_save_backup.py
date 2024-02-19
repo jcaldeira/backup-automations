7ac6db45ebd9f34b2322857531edf83d026c1e97
JCaldeira <50929436+jcaldeira@users.noreply.github.com>
2024-02-19 12:35:24 +0000

    Added user input Formated variable names to be more uniform Added a condition to delete older backups

--- ArkSE single player/ark_solo_save_backup.py
+++ ArkSE single player/ark_solo_save_backup.py
@@ -2,23 +2,58 @@ import os
 import shutil
 from datetime import datetime
 
+#region Ask if the script is supose to run
+UserInput = ' '
+
+while (UserInput.lower() not in ['y', 'n', '']):
+    UserInput = input('Run Ark: SE single player backup script? (Y/n) ')
+
+if (UserInput.lower() == 'n'):
+    RunScript = False
+else:
+    RunScript = True
+
+#endregion Ask if the script is supose to run
+
+#region Create paths and timestamps for backup
 Timestamp = datetime.strftime(datetime.now(), '%Y-%m-%d_%H-%M-%S')
 DestinationPath = os.path.join(str(os.getenv('OneDrive')), r'Game Saves\Ark\Single Player\Backups\Automatic')
 
-Archive_base_name = os.path.join(DestinationPath, Timestamp)
-Archive_format = 'zip'
-Archive_root_dir = r'C:\Program Files (x86)\Steam\steamapps\common\ARK\ShooterGame'
-Archive_base_dir = r'Saved'
-Archive_dry_run = False
+ArchiveBaseName = os.path.join(DestinationPath, Timestamp)
+ArchiveFormat = 'zip'
+ArchiveRootDir = r'C:\Program Files (x86)\Steam\steamapps\common\ARK\ShooterGame'
+ArchiveBaseDir = r'Saved'
+ArchiveDryRun = False
 
-ArkLocalSavePath = os.path.join(Archive_root_dir, Archive_base_dir)
+ArkLocalSavePath = os.path.join(ArchiveRootDir, ArchiveBaseDir)
 
-if (os.path.exists(ArkLocalSavePath)):
-    print('Ark saves exist. Creating an archive of tree structure.')
+#endregion Create paths and timestamps for backup
+
+#region Make sure backup folder is not cluttered
+BackupsInFolder = os.listdir(DestinationPath)
+
+OldestBackupPath = os.path.join(DestinationPath, BackupsInFolder[0])
+
+# OldestBackupTimestamp = datetime.fromtimestamp(os.stat(OldestBackupPath).st_birthtime)
+# OldestTimeDelta = (datetime.now() - OldestBackupTimestamp).days
+
+# if ((len(BackupsInFolder) > 30) and (OldestTimeDelta > 180)):
+if (len(BackupsInFolder) > 30):
+    os.remove(OldestBackupPath)
+
+#endregion Make sure backup folder is not cluttered
+
+#region Create backup
+if (os.path.exists(ArkLocalSavePath) and (RunScript)):
+    print('Creating backup now.')
     shutil.make_archive(
-        base_name=Archive_base_name,
-        format=Archive_format,
-        root_dir=Archive_root_dir,
-        base_dir=Archive_base_dir,
-        dry_run=Archive_dry_run
+        base_name=ArchiveBaseName,
+        format=ArchiveFormat,
+        root_dir=ArchiveRootDir,
+        base_dir=ArchiveBaseDir,
+        dry_run=ArchiveDryRun
     )
+
+#endregion Create backup
+
+input('Press ENTER to close this window.')
